6f9e13969dfaef08fb659e15e5f23a7b6c4af9ab
Hugo Rivero <Hugo200440@gmail.com>
2019-02-12 15:08:32 -0430

    protocolo update

--- layouts/base.js
+++ layouts/base.js
@@ -4,7 +4,7 @@ export default ({ children }) => (
 	<div className = "main">
 		<div className="logo">
 		</div>
-
+		{/* ---------- Hugo Rivero 12/02/19 ------------ */}
 		{ children }
 	</div>
 )
